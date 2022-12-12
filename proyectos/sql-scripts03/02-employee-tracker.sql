9f4dbcf9fc07898bb8574f603c6b2cc0b2431032
Miguel Rugerio <mrugerio@gmail.com>
2022-12-12 13:02:13 -0600

    Se actualizo nombre de la tabla

--- proyectos/sql-scripts03/02-employee-tracker.sql
+++ proyectos/sql-scripts03/02-employee-tracker.sql
@@ -24,7 +24,7 @@ USE `employee_directory`;
 DROP TABLE IF EXISTS `employee`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `customer` (
+CREATE TABLE `employee` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
